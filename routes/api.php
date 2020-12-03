4e50ebaf4aa94c2285231689a18ba5a3d249a3b6
seli <>
2020-12-03 16:58:40 +0700

    user default

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,14 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 Route::get('email/verify/{id}', 'API\VerificationApiController@verify')->name('verificationapi.verify');
-// Route::get('email/resend', 'API\VerificationApiController@resend')->name('verificationapi.resend');
 
-Route::group(['middleware' => 'auth:api'], function(){
+Route::group(['middleware' => 'auth:api','verified'], function(){
     Route::post('details', 'API\UserController@details');
     Route::post('logout', 'API\UserController@logout');
 });
 
 Route::group([
-    'middleware' => 'auth:api'
+    'middleware' => 'auth:api' , 'verified'
 ], function () {
 
     Route::group([    
