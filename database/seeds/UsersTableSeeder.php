059b18e099f85b7f82c7765d47879ab1591fdd39
seli <>
2020-12-03 09:00:02 +0700

    first commit new

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\User::create([
+            'id'    => '1',
+            'name'  => 'seli',
+            'email' => 'susantiseli03@gmail.com',
+            'password'  => bcrypt('12345678')
+        ]);
+    }
+}
